c7146e9b69cd9d527b58b404764eb7f96f6b5147
Maxime Menant <maxime.menant@intellicore.net>
2015-04-10 16:54:10 +0200

    Set channel and username response

--- MemeBot.php
+++ MemeBot.php
@@ -8,7 +8,9 @@ $meme = new MemeBot\MemeClass;
 
 // Get Request
 $trigger = trim($_POST['trigger_word']);
-$text    = trim(substr($_POST['text'], strlen($trigger) + 1));
+$text    = trim(substr($_POST['text'], strlen($trigger)));
+$channel = trim($_POST['channel_name']);
+$user    = trim($_POST['user_name']);
 
 if($text == '') {
 	sendResponse("One does not simply <text> - Lord of the Rings Boromir\nY U NO <text> - Y U NO Guy\nI don't always <text> but when i do <text> - The Most Interesting man in the World\nNot sure if <text> or <text> - Futurama Fry\nBrace yourselves <text> - Brace Yourselves X is Coming (Imminent Ned, Game of Thrones)\nYo dawg <text> so <text> - Yo Dawg Heard You (Xzibit)\n<text> that would be great - Bill Lumbergh from Office Space\nWhat if I told you <text> - Matrix Morpheus");
@@ -40,9 +42,11 @@ function sendResponse($response)
 // (string) $message - message to be passed to Slack
 // (string) $room - room in which to write the message, too
 // (string) $icon - You can set up custom emoji icons to use with each message
-function slack($message) {
+function slack($message, $username, $channel) {
   $data = "payload=" . json_encode(array(
           "text" =>  urlencode("<".$message.">"),
+          "username" => $username,
+          "channel"  => $channel
       ));
 
   // You can get your webhook endpoint from your Slack settings
@@ -52,6 +56,8 @@ function slack($message) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $result = curl_exec($ch);
   curl_close($ch);
+
+  Log::info("Sent to Slack: " . $data, array('context' => 'Notifications'));
 
   return $result;
 }
