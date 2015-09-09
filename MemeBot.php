e40819d1d2910990ec0dc58ea693d384c9fb5a8f
KinanIntellicore <kinan.arnaout@intellicore.net>
2015-09-09 11:00:20 +0200

    Adding new gifs

--- MemeBot.php
+++ MemeBot.php
@@ -44,6 +44,8 @@ if($text == '') {
     On en a gros - On en a gros gif
     Mind blown - Mind blown gif
     crash - Crash gif
+    wtf - What the fuck gif Colbert
+    magic - magic gif Shia
     ");
 }
 
